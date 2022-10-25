66e6374ee469d3140f846e52291affceee80aae9
Aicha Nouisser <aicha.nouisser@esprit.tn>
2022-10-25 23:54:52 +0200

    first push

---
+++ app/src/main/java/com/example/aichaff/StartWork.java
@@ -0,0 +1,14 @@
+package com.example.aichaff;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartWork extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_work);
+    }
+}
